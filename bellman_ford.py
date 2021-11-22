6d81fa5d67e6f39cc243ccf98717d81ccbfb5eaa
Julissa Ponte <66703701+JPonte09@users.noreply.github.com>
2021-11-21 23:33:10 -0500

    Create bellman_ford.py
    Create bellman_ford.py close #19

---
+++ bellman_ford.py
@@ -0,0 +1,30 @@
+#-----IMPLEMENTACION DE PRIMERA VERSION DEL ALGORITMO BELLMAN FORD------
+import math
+
+def bellmanFord(G, s):
+  n = len(G)
+  cost = [float('inf')]*n
+  parent = [None]*n
+  
+  cost[s] = 0
+  
+  # relax
+  for _ in range(n-1):
+    for current in range(n):
+      for v in G[current]:
+        if cost[current] + 1 < cost[v]:
+          cost[v] = cost[current] + 1
+          parent[v] = current
+
+  # check negative cycle
+  for current in range(n):
+    for v in G[current]:
+      if cost[current] + 1 < cost[v]:
+        return None, None
+
+  return parent, cost
+
+parent, cost = bellmanFord(grafoCiudad, 10)
+
+print(parent)
+print(cost)
