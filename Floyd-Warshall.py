5e7d95c7ca8089a5018d7599e0af539167af646b
Sebastian Trujillo <74939920+SebastianTrujillo21@users.noreply.github.com>
2021-11-22 00:17:49 -0500

    Update Floyd-Warshall close #18

--- Floyd-Warshall.py
+++ Floyd-Warshall.py
@@ -1,1 +1,19 @@
-
+def floydWarshall(G, tamano):
+    n=len(G)
+    Costos = [[math.inf]*tamano for _ in range(tamano)]
+    Padres = [[-1]*tamano for _ in range(tamano)]
+    for nodos in range(n):
+        for distancia, nodo, vecino in G[nodos]:
+            Costos[nodo][nodo] = 0
+            Costos[nodo][vecino] = distancia
+            Padres[nodo][vecino] = nodo
+            
+    for k in range(tamano):
+        for i in range(tamano):
+            for j in range(tamano):
+                pesoAcumulado = Costos[i][k] + Costos[k][j]
+                if Costos[i][j] > pesoAcumulado:
+                    Costos[i][j] = pesoAcumulado
+                    Padres[i][j] = k
+           
+    return Padres
