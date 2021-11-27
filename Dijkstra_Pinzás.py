76a4a7edd698d7991ba6f5859fc4b7c1740d810d
LoelDev <84813731+LoelDev@users.noreply.github.com>
2021-11-27 17:04:09 -0500

    close #24 close #31

--- Dijkstra_Pinzás.py
+++ Dijkstra_Pinzás.py
@@ -1,4 +1,36 @@
-def Tercer_Algoritmo(g, s):
+import math
+
+def leer_archivo(archivo):
+  archivoCsv = list()
+  with open(archivo) as a: 
+    reader = csv.reader(a, delimiter=',', quotechar=',', quoting=csv.QUOTE_MINIMAL)
+    for row in reader:
+        if row[0] != 'x':
+          row[0], row[1] = int(row[0]), int(row[1])
+          archivoCsv.append(row)
+  archivoCsv.sort(key = lambda x: (x[0], x[1]))
+  return archivoCsv
+
+almacenes = leer_archivo("almacenes.csv")
+puntos = leer_archivo("puntos.csv")
+
+for i,_ in enumerate(ciudad):
+  if ciudad[i][2] == 'A':
+    almacenes.append(ciudad[i])
+
+for j,_ in enumerate(ciudad):
+  if ciudad[j][2] == 'P':
+    puntos_de_entrega.append(ciudad[j])
+
+grafoCiudad=[[] for _ in range(2700)]
+for i,_ in enumerate(ciudad):
+      grafoCiudad[ciudad[i][0]].append(ciudad[i][1])
+
+
+#----------Algoritmo 4 - Dijkstra----------
+
+
+def Dijkstra(G, s):
   visited = {}
   path = {}
   cost = {}
@@ -13,10 +45,12 @@ def Tercer_Algoritmo(g, s):
     u, g_u = hq.heappop(queue)
     if not visited[u]:
       visited[u] = True        
-      for v in g[u]:
-        f = g_u + 1
+      for v in G[u]:
+        f = G_u + 1
         if f < cost[v]:
           cost[v] = f
           path[v] = u
           hq.heappush(queue, (v, f))
   return path
+
+Dijkstra(ciudad, almacenes)
